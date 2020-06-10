61c0f6641bc1b8fe0aa57e4a7c0408123b9048f6
Babatunde Okediran <btundeokediran@gmail.com>
2020-06-10 18:54:31 +0100

    Initial commit

---
+++ main.dart
@@ -0,0 +1,56 @@
+void main() {
+  var deck = new Deck();
+  print(deck);
+//   print(deck.deal(5));
+  deck.removeCard("Diamonds", "Ace");
+  print(deck);
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ["Ace", "Two", "Three", "Four", "Five"];
+    var suits = ["Diamonds", "Hearts", "Clubs", "Spades"];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(suit: suit, rank: rank);
+        cards.add(card);
+      }
+    }
+  }
+
+  toString() {
+    return cards.toString();
+  }
+
+  shuffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuit(String suit) {
+    return cards.where((card) => card.suit == suit);
+  }
+  
+  deal(int handSize){
+    var hand = cards.sublist(0, handSize);
+    cards = cards.sublist(handSize);
+    return hand;
+  }
+  
+  removeCard(String suit, String rank){
+    cards.removeWhere((card) => card.suit == suit && card.rank == rank);
+  }
+}
+
+class Card {
+  String suit;
+  String rank;
+
+  Card({this.suit, this.rank});
+
+  toString() {
+    return "$rank of $suit";
+  }
+}
